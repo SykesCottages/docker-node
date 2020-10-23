1294b907cd84896940549a34ad61540018f1b10c
James Lockhart <james.lockhart@sykescottages.co.uk>
2020-10-23 18:22:17 +0100

    Added node 14 and 15

---
+++ 14/run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+itShouldCheckThatAllInstalledSoftwareExists() {
+    doesCommandExist node
+    doesCommandExist npm
+}
+
+itShouldMatchTheDesiredVersions() {
+  doesMatchVersion node v14.9.0
+  doesMatchVersion npm 6.14.8
+}
+
+doesCommandExist() {
+    if ! [ -x "$(command -v $1)" ]; then
+      echo "Error: $1 is not installed." >&2
+      exit 1
+    fi
+}
+
+doesMatchVersion() {
+    VERSION=$($1 -v|grep -i "$2")
+    if [ -z "$VERSION" ]; then
+      echo "Error: $1 version $2 is not found." >&2
+      exit 1
+    fi
+}
+
+itShouldCheckThatAllInstalledSoftwareExists
+itShouldMatchTheDesiredVersions
