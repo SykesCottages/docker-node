442b6dd5d7791a09f08708714e55aa28b2c0a8e4
James Lockhart <james.lockhart@sykescottages.co.uk>
2021-10-06 17:04:34 +0100

    Added the latest and the base containers

--- dockerhub.sh
+++ dockerhub.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Validate the container passes our tests
-./test.sh
+#./test.sh
 
 # You need to provide your own creds because #security
 docker login
+
+docker build --no-cache -t sykescottages/node:base $VERSION
+docker push sykescottages/node:base
 
 VERSIONS=( 4 5 6 7 8 9 10 11 12 13 14 15 16 )
 for VERSION in "${VERSIONS[@]}"
@@ -12,3 +15,6 @@ do
   docker build --no-cache -t sykescottages/node:${VERSION} $VERSION
   docker push sykescottages/node:${VERSION}
 done
+
+docker build --no-cache -t sykescottages/node:latest 16
+docker push sykescottages/node:latest
