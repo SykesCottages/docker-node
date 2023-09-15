1d898bdddbdf68c96249a1c79ea48365ea6500a6
James Lockhart <james.lockhart@sykescottages.co.uk>
2023-09-15 09:52:55 +0100

    Remove the echo in the versions

--- versions.sh
+++ versions.sh
@@ -4,6 +4,5 @@ VERSIONS=($(find . -type d -regex '\.*/[0-9]+' | sort | sed 's/\.\///'))
 
 for VERSION in "${VERSIONS[@]}"
 do
-  echo $VERSION
   docker run -it sykescottages/node:${VERSION} bash -c "apt -qq list | tail -n+2" | tail -n+4 > $VERSION/aptitude-itinerary
 done
