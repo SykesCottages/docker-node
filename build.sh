68d3bcfae85c29e1c1933b8410a0bbd0991143c4
James Lockhart <james.lockhart@sykescottages.co.uk>
2023-12-04 13:43:08 +0000

    Ensure ancestry is respected for multi arch

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ ARCH=$2
 
 TAG="sykescottages/node:${VERSION}-${ARCH}"
 
-docker build --quiet --no-cache -t $TAG $VERSION
+docker build --quiet --no-cache -t $TAG --build-arg ARCH=$ARCH $VERSION
 docker push $TAG
