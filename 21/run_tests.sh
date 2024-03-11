32fc692c07bbf543ba03d6bcff57c89772aa5d92
James Lockhart <james.lockhart@sykescottages.co.uk>
2024-03-11 18:45:56 +0000

    Upgraded node 21 to 21.7.1

--- 21/run_tests.sh
+++ 21/run_tests.sh
@@ -6,7 +6,7 @@ itShouldCheckThatAllInstalledSoftwareExists() {
 }
 
 itShouldMatchTheDesiredVersions() {
-  doesMatchVersion node v21.6.1
+  doesMatchVersion node v21.7.1
   doesMatchVersion npm 10.2.4
 }
 
