b31e9547e32932a1085a792b426d63cce9b11623
RobHarveyDev <87716995+RobHarveyDev@users.noreply.github.com>
2025-11-21 15:09:17 +0000

    Add node 24 and 25

---
+++ 24/run_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+itShouldCheckThatAllInstalledSoftwareExists() {
+    doesCommandExist node
+    doesCommandExist npm
+}
+
+itShouldMatchTheDesiredVersions() {
+  doesMatchVersion node v24.11.1
+  doesMatchVersion npm 11.6.2
+}
+
+doesCommandExist() {
+    if ! [ -x "$(command -v $1)" ]; then
+      echo "Error: $1 is not installed." >&2
+      exit 1
+    fi
+}
+
+doesMatchVersion() {
+    VERSION=$($1 -v|grep -i "$2")
+    if [ -z "$VERSION" ]; then
+      echo "Error: $1 version $2 is not found, found $($1 -v) instead." >&2
+      exit 1
+    fi
+}
+
+itShouldCheckThatAllInstalledSoftwareExists
+itShouldMatchTheDesiredVersions
